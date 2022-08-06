2d64f96fd4060e4a0553bffdd1b4b9ac26ed52ec
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-08-06 19:47:41 +0900

    fix bug at Vote Entity Constructor

--- airconditioner/src/main/java/com/example/airconditioner/entity/Vote.java
+++ airconditioner/src/main/java/com/example/airconditioner/entity/Vote.java
@@ -4,20 +4,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@RequiredArgsConstructor
 @Entity
 public class Vote {
+    public Vote(String evaluation) {
+    }
+
+    public Vote() {
+    }
+
     @Id
     @GeneratedValue
     Long seq;
-    @NonNull
     String evaluation;
     String cid;
     String uid;
